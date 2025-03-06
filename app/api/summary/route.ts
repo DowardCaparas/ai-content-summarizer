c8ca8787c4aa32fab00e9fc63c5c56d5074667ad
DowardCaparas <caparasdounhuward@gmail.com>
2025-03-06 12:26:38 +0800

    add API key secret

--- app/api/summary/route.ts
+++ app/api/summary/route.ts
@@ -2,7 +2,9 @@ import { NextResponse } from "next/server";
 import OpenAI from "openai";
 
 async function getCompletion() {
-    const openai = new OpenAI();
+    const openai = new OpenAI({
+      apiKey: process.env.OPENAI_API_KEY!,
+    });
     const completion = await openai.chat.completions.create({
         model: "gpt-4o",
         messages: [
